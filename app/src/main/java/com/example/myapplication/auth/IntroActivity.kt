da5ee9870a64a03d037fbb3ebf03d04e24225215
OckJuWon0831 <ockjuwon99@naver.com>
2023-11-04 22:32:59 +0800

    Feat: Google login added

--- app/src/main/java/com/example/myapplication/auth/IntroActivity.kt
+++ app/src/main/java/com/example/myapplication/auth/IntroActivity.kt
@@ -1,14 +1,23 @@
 package com.example.myapplication.auth
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityIntroBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -16,11 +25,19 @@ class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
         auth = Firebase.auth
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         binding.loginBtn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -30,6 +47,10 @@ class IntroActivity : AppCompatActivity() {
         binding.signinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.googleBtn.setOnClickListener {
+            googleSignIn();
         }
 
         binding.nonloginBtn.setOnClickListener {
@@ -43,6 +64,35 @@ class IntroActivity : AppCompatActivity() {
                         Toast.makeText(this, "Acess failed", Toast.LENGTH_LONG).show()
                     }
                 }
+        }
+    }
+
+    private fun googleSignIn() {
+        val signInClient = googleSignInClient.signInIntent
+        launcher.launch(signInClient)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                manageResults(task)
+            }
+    }
+
+    private fun manageResults(task: Task<GoogleSignInAccount>) {
+        val account : GoogleSignInAccount? = task.result
+        if(account != null) {
+            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+            auth.signInWithCredential(credential).addOnCompleteListener {
+                if(task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Google login failed", Toast.LENGTH_LONG).show()
+                }
+            }
         }
 
     }
